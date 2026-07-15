bbfabc0144990c9b6b349ddd809244fa9c57452a
Raphael Menyawovor <raphaelmenyawovor@gmail.com>
2026-07-15 09:23:11 +0000

    fix: add return type to bootstrap function in main.ts to fix missing return type

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { AppModule } from './app.module';
 
 import { NestFactory } from '@nestjs/core';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
